b3109d21afd4e432eea1ae8b45df40437516ea27
rupinderkaur30-gif <rupi063094@gmail.com>
2021-03-03 15:06:06 -0500

    commit tree 5a4c77df61b2f1ed6efd23de2ea0d57fc5b40572

---
+++ lib/class_methods_module.rb
@@ -0,0 +1,16 @@
+
+module MetaDancing
+
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end 
+
+
+
+module ClassMethods
+  
+    def metadata
+      "This class produces objects that love to dance."
+    end
+  end
